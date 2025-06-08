ebd8f645f17f42b38a3837990d219a3d5975b539
Charles T. Gray <charlestigray@gmail.com>
2025-06-08 20:28:54 +0200

    I think this was the last iteration of the R function for dotdot

---
+++ colog/pipeline.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# filepath: /home/cantabile/Documents/repos/good-enough-playpen/colog/dot2svg.sh
+
+# Convert pipeline.dot to pipeline.svg using Graphviz
+dot -Tsvg colog/pipeline.dot -o colog/pipeline.svg
+
+
+
